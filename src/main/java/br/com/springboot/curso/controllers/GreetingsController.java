219d723ccdc0ec003c5dbb0ebe5e0e9102f1af7e
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-10-31 14:48:36 -0300

    Delete

--- src/main/java/br/com/springboot/curso/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso/controllers/GreetingsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,7 +71,15 @@ public class GreetingsController {
   
     }
     
-    
+    @DeleteMapping(value = "delete")
+    @ResponseBody 							 /*requestparam pega um parametro*/
+    public ResponseEntity<String> delete (@RequestParam Long iduser) {
+    	
+    	usuarioRepository.deleteById(iduser);
+    	
+    	return new ResponseEntity<String>("Usuario deletado",HttpStatus.OK);
+  
+    }
     
     
     
